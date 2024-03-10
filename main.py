85b2cdec824c0008d2bc383d9d0505411588832a
RomanTuras <salden.com@gmail.com>
2024-03-10 16:13:48 +0200

    First commit

---
+++ main.py
@@ -0,0 +1,19 @@
+from prompt_toolkit import prompt
+from prompt_toolkit import print_formatted_text, HTML
+
+print_formatted_text(HTML('<b>This is bold</b>'))
+print_formatted_text(HTML('<i>This is italic</i>'))
+print_formatted_text(HTML('<u>This is underlined</u>'))
+
+# Colors from the ANSI palette.
+print_formatted_text(HTML('<ansired>This is red</ansired>'))
+print_formatted_text(HTML('<ansigreen>This is green</ansigreen>'))
+
+# Named colors (256 color palette, or true color, depending on the output).
+print_formatted_text(HTML('<skyblue>This is sky blue</skyblue>'))
+print_formatted_text(HTML('<seagreen>This is sea green</seagreen>'))
+print_formatted_text(HTML('<violet>This is violet</violet>'))
+
+if __name__ == '__main__':
+    answer = prompt('Give me some input: ')
+    print('You said: %s' % answer)
---
+++ setup.py
@@ -0,0 +1,3 @@
+setup(name='pythontest', 
+    version='1.0', 
+    packages=find_packages(include=['prompt_toolkit']))
